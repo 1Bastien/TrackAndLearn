695ac6cbc646f6bcbbe79046f223b4f5308c3e2a
Bastien <bastien.brgt@protonmail.com>
2025-06-21 14:42:23 +0200

    feat: backend initialization

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend Track&Learn - API Running" });
+});
+
+app.listen(PORT);
